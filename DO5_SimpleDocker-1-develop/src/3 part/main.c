6f34e95f2a81fadd274fb807d2ad11414bd9c4cc
Albert <103403437+Morty00933@users.noreply.github.com>
2024-09-10 17:50:13 +0300

    Add files via upload

---
+++ DO5_SimpleDocker-1-develop/src/3 part/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    printf("Content-Type: text/html\n\n");
+    printf("<html><head><title>Hello World!</title></head><body><h1>Hello World!</h1></body></html>\n");
+    return 0;
+}
